34f5e0d977c8c16f24b16827f58e8e03d57dc413
Zetzher <j.abasolo.agenjo@gmail.com>
2020-06-05 00:29:22 +0200

    Done, faltan estilos

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
     EuiCard,
     EuiIcon,
@@ -11,7 +11,7 @@ import {
 
 const List = ({municipios, almacen}) => {
 
-
+    const [datosMunicipio, guardarDatosMunicipio] = useState();
     
 
     const getTiempo = async (codProv, codMun) => {
@@ -21,9 +21,12 @@ const List = ({municipios, almacen}) => {
        
         let result = codMun.substr(0, 5);
        
-       const response = await axios.get(`https://www.el-tiempo.net/api/json/v2/provincias/${codProv}/municipios/${result}`)
-       console.log('holis', response.data)
+       const response = await axios.get(`https://www.el-tiempo.net/api/json/v2/provincias/${codProv}/municipios/${result}`);
+       const datosMunicipio = response.data;
+       console.log(datosMunicipio)
+       guardarDatosMunicipio(datosMunicipio);
         //Municipio, temperatura actual y probabilidad de lluvia
+        //cielo stateSky.description -- humedad -- nombre municipio.NOMBRE -- temperatura temperaturas.mas y temperaturas.min -- lluvia lluvia %
       }
     
     catch (err) {
@@ -31,7 +34,6 @@ const List = ({municipios, almacen}) => {
     }
   }
 
-  console.log(municipios)
 
   useEffect(() => {
     getTiempo()
@@ -41,7 +43,8 @@ const List = ({municipios, almacen}) => {
             <>
               <div>
                 {municipios ? municipios.map((municipios) => 
-                  <EuiFlexGroup gutterSize="l">
+                
+                  <EuiFlexGroup gutterSize="l" >
                   
                     <EuiFlexItem>
                       <EuiCard
@@ -52,9 +55,18 @@ const List = ({municipios, almacen}) => {
                       />
                     </EuiFlexItem>
                   </EuiFlexGroup>) : null}
+                
               </div>
               <div>
-
+                  {datosMunicipio ? (
+                    <>
+                      <div>{datosMunicipio.municipio.NOMBRE}</div>
+                      <span>Temp. Max{datosMunicipio.temperaturas.max}º Temp. Min{datosMunicipio.temperaturas.min}º</span>
+                      <div>{datosMunicipio.stateSky.description}</div>
+                      <div>Humedad: {datosMunicipio.humedad}%</div>
+                </>)
+                : null
+                  }
               </div>
             </>
         )
